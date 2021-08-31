1830a1c3dcd5ca56e817ec2dd110778c5ab1feb4
Crowdbotics <team@crowdbotics.com>
2021-08-31 17:47:41 +0000

    Update models

---
+++ backend/course/api/v1/viewsets.py
@@ -0,0 +1,127 @@
+from rest_framework import authentication
+from course.models import (
+    PaymentMethod,
+    Recording,
+    Category,
+    Lesson,
+    Enrollment,
+    SubscriptionType,
+    Module,
+    Group,
+    Course,
+    Subscription,
+    Event,
+)
+from .serializers import (
+    PaymentMethodSerializer,
+    RecordingSerializer,
+    CategorySerializer,
+    LessonSerializer,
+    EnrollmentSerializer,
+    SubscriptionTypeSerializer,
+    ModuleSerializer,
+    GroupSerializer,
+    CourseSerializer,
+    SubscriptionSerializer,
+    EventSerializer,
+)
+from rest_framework import viewsets
+
+
+class ModuleViewSet(viewsets.ModelViewSet):
+    serializer_class = ModuleSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Module.objects.all()
+
+
+class LessonViewSet(viewsets.ModelViewSet):
+    serializer_class = LessonSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Lesson.objects.all()
+
+
+class PaymentMethodViewSet(viewsets.ModelViewSet):
+    serializer_class = PaymentMethodSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = PaymentMethod.objects.all()
+
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    serializer_class = EnrollmentSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Enrollment.objects.all()
+
+
+class SubscriptionViewSet(viewsets.ModelViewSet):
+    serializer_class = SubscriptionSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Subscription.objects.all()
+
+
+class SubscriptionTypeViewSet(viewsets.ModelViewSet):
+    serializer_class = SubscriptionTypeSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = SubscriptionType.objects.all()
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    serializer_class = GroupSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Group.objects.all()
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Course.objects.all()
+
+
+class RecordingViewSet(viewsets.ModelViewSet):
+    serializer_class = RecordingSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Recording.objects.all()
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    serializer_class = EventSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Event.objects.all()
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Category.objects.all()
---
+++ backend/course/views.py
@@ -0,0 +1,17 @@
+from .models import (
+    PaymentMethod,
+    Recording,
+    Category,
+    Lesson,
+    Enrollment,
+    SubscriptionType,
+    Module,
+    Group,
+    Course,
+    Subscription,
+    Event,
+)
+from django.views.generic import CreateView, UpdateView, DetailView, ListView
+from django.shortcuts import render
+
+# Create your views here.
